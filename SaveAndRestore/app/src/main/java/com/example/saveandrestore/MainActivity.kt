cc5035341571cab32e64835372ced8165b16d9d1
AngelBrambila91 <malthael.91@gmail.com>
2023-09-05 12:26:31 -0600

    Finished Fron-t End on SaveAndRestore Project

--- SaveAndRestore/app/src/main/java/com/example/saveandrestore/MainActivity.kt
+++ SaveAndRestore/app/src/main/java/com/example/saveandrestore/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.saveandrestore
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +16,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.saveandrestore.ui.theme.SaveAndRestoreTheme
 
 class MainActivity : ComponentActivity() {
+    private val discountButton : Button get() = findViewById(R.id.discount_button)
+    private val firstName : EditText get() = findViewById(R.id.first_name)
+    private val lastName : EditText get() = findViewById(R.id.last_name)
+    private val email : EditText get() = findViewById(R.id.email)
+    private val discountCodeConfirmation : TextView get() = findViewById(R.id.discount_code_confirmation)
+    private val discountCode : TextView get() = findViewById(R.id.discount_code)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
